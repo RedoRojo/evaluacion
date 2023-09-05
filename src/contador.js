2430e07fdfb1c6f8c1cadb749c9b167771639816
Cristhian Victor Rojas Marquez <cristhiabnvictorrm@gmail.com>
2023-09-05 10:04:43 -0400

    Se identifica las palabras separadar por espacio en el texto: 'el password de'

---
+++ src/contador.js
@@ -0,0 +1,14 @@
+function getPalabras(texto) {
+    let palabras = []; 
+    let palabra  = ""; 
+    for(let i = 0; i<texto.length; i++) {
+        if(texto[i] == " ") {
+            palabras.push(palabra);
+            palabra = ""; 
+        } else palabra += texto[i];
+    }
+    if(palabra.length > 0) palabras.push(palabra);
+    return palabras; 
+}
+
+export {getPalabras};
--- src/contador.test.js
+++ src/contador.test.js
@@ -1,12 +1,13 @@
-
+import { getPalabras } from "./contador";
 
 
 describe("Contador de frases", () => {
     it("Deberia poder identificar las palabras en la frase: 'el password' que estan separadas por un espacio", () => {
       expect(getPalabras("el password")).toEqual(["el", "password"]);
     });
+
+    it("Deberia poder identificar las palabras en la frase: 'el password de' que estan separadas por un espacio", () => {
+        expect(getPalabras("el password de")).toEqual(["el", "password", "de"]);
+      });
   });
   
-function getPalabras(texto) {
-    return ["el", "password"]; 
-}
